6aebe1a973b28675edbe51300d12193cdceadec5
Andros Slowley <andros@codepath.org>
2025-02-12 15:55:57 -0500

    Starting Fresh

--- TheMorningApp/ViewController.swift
+++ TheMorningApp/ViewController.swift
@@ -9,48 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var wordsButton: UIButton!
-    
-    @IBOutlet weak var quotesButton: UIButton!
-    
-    @IBOutlet weak var affirmationButton: UIButton!
-    
-    var currentSelection: MorningMessageType = .Word
-    
-    @IBOutlet weak var currentMessage: UILabel!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    
-    @IBAction func didSelectAction(_ sender: Any) {
-        guard let button = sender as? UIButton else { return }
-        
-        guard let titleText = button.titleLabel?.text else { return }
-        
-        guard let message = MorningMessageType(rawValue: titleText) else { return }
-        
-        currentSelection = message
-        currentMessage.text = currentSelection.messages().first!
-    }
-    
-    /*
-    @IBAction func didSelectWords(_ sender: Any) {
-        currentSelection = .Word
-        currentMessage.text = currentSelection.messages().first!
-    }
-    
-    @IBAction func didSelectQuotes(_ sender: UIButton) {
-        currentSelection = .Quote
-        currentMessage.text = currentSelection.messages().first!
-    }
-    
-    @IBAction func didSelectAffirmation(_ sender: UIButton) {
-        currentSelection = .Affirmation
-        currentMessage.text = currentSelection.messages().first!
-    }
-   */
-}
 
-// https://www.pexels.com/photo/snow-covered-mountain-2086620/
+
+}
